13497a39569b71d58786c951e98e89e65a9cbfab
Alcides A <aaugusto.kd@gmail.com>
2020-10-26 20:26:56 -0300

    add board controller

---
+++ src/main/java/com/alcides/ollertservicejava/controller/BoardController.java
@@ -0,0 +1,33 @@
+package com.alcides.ollertservicejava.controller;
+
+import com.alcides.ollertservicejava.entity.Board;
+import com.alcides.ollertservicejava.entity.User;
+import com.alcides.ollertservicejava.repository.UserRepository;
+import com.alcides.ollertservicejava.service.impl.AuthenticationFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/boards")
+public class BoardController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private AuthenticationFacade authenticationFacade;
+
+    @PostMapping
+    public void add(@RequestBody Board board) {
+
+        User user = authenticationFacade.getAuthenticatedUser();
+        
+        user.getBoards().add(board);
+
+        userRepository.save(user);
+    }
+
+}
